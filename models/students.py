183fa05eec9a23138fab6bd0770df88a3fa9481c
kaytee07 <kofi.taylor07@gmail.com>
2023-09-02 10:44:52 +0000

    create student data model

---
+++ models/students.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+creat user class that inherit from BaseModel
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.types import LargeBinary
+from sqlalchemy.orm import relationship
+from os import getenv
+
+SD_TYPE_STORAGE = getenv('SD_TYPE_STORAGE')
+
+class User(BaseModel, Base):
+    """
+    user class that describes the user
+    """
+    __tablename__ = 'students'
+
+    if SD_TYPE_STORAGE == 'db':
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        course_id = Column(String(128), ForeignKey('courses.id'))
+        cohort_id = Column(String(128), ForeignKey('cohorts.id'))
+        Phone_no = Column(Integer, nullable=False)
+        age = Column(Integer, nullable=False)
+        email = Column(String(60), nullable=False)
+    else:
+        first_name = None
+        last_name = None
+        course_id = None
+        cohort_id = None
+        Phone_no = None
+        age = None
+        email = None
+
+    def __init__(self, *args, **kwargs):
+        """
+        inherit BaseModel methods and attributes
+        """
+        super().__init__(*args, **kwargs)
