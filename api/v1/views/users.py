e711117470bc4f2befb13b3a34665496c2153669
kaytee07 <kofi.taylor07@gmail.com>
2023-09-03 04:18:22 +0000

    login sucessfully implemented

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,8 +3,9 @@
 all user apis including login and logout page
 """
 
-from flask import abort, render_template, request, jsonify
+from flask import abort, render_template, request, jsonify, session
 from api.v1.views import app_views
+from models import storage
 from models.users import User
 import os
 import hashlib
@@ -27,31 +28,35 @@ def hash_password(password, salt=None):
         return hashed_password
 
 
-@app_views.route('/createuser', strict_slashes="False", methods=['GET', 'POST'])
+@app_views.route('/createuser', strict_slashes=False, methods=['GET', 'POST'])
 def createuser():
     """
     create user who can check registered kids
     """
-    data = request.get_json()
+    if request.method == 'POST':
+        data = request.get_json()
 
-    if not data:
-        abort(404, description="absolutely no data")
+        if not data:
+            abort(404, description="absolutely no data")
 
-    if 'password' not in data:
-        abort(404, description="No password")
+        if 'password' not in data:
+            abort(404, description="No password")
 
-    if 'email' not in data:
-        abort(404, description="No email")
+        if 'email' not in data:
+            abort(404, description="No email")
 
-    if 'phone_no' not in data:
-        abort(404, description="No phone_no")
+        if 'phone_no' not in data:
+            abort(404, description="No phone_no")
 
-    hashed_data = hash_password(data['password'])
-    data['password'] = hashed_data['passwd']
-    data['salt'] = hashed_data['salt']
-    new_user = User(**data)
-    new_user.save()
-    return jsonify(new_user.to_dict())
+        hashed_data = hash_password(data['password'])
+        data['password'] = hashed_data['passwd']
+        data['salt'] = hashed_data['salt']
+        print(data)
+        new_user = User(**data)
+        new_user.save()
+        return jsonify(new_user.to_dict())
+    else:
+        return jsonify({"return": "success"})
 
 
 @app_views.route('/login', strict_slashes=False, methods=['GET', 'POST'])
@@ -59,4 +64,25 @@ def login():
     """
     login as user online
     """
-    pass
+    if request.method == 'POST':
+        data = request.get_json()
+
+        if not data:
+            abort(404, description="absolutely no data")
+
+        if 'password' not in data:
+            abort(404, description="No password")
+
+        if 'username' not in data:
+            abort(404, description="No username")
+
+        user = storage.get(User, username=data['username'])
+        user_dict = user.to_dict()
+
+        if user:
+            if hash_password(data['password'], user_dict['salt']) == user_dict['password']:
+                return jsonify({"login": "successfully"}), 200
+            else:
+                abort(404, description="incorrect username and password")
+        else:
+            abort(404, description="incorrect username and password")
