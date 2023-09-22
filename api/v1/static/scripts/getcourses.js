ba66b365e20b8e53ecae63246e02044987838d45
kaytee07 <kofi.taylor07@gmail.com>
2023-09-22 14:06:09 +0000

    fetch delete api from backend

---
+++ api/v1/static/scripts/getcourses.js
@@ -0,0 +1,37 @@
+const trash = document.querySelector('svg.trash');
+const edit = document.querySelector('svg.edit');
+const user = document.querySelector('div.student_details');
+
+trash.addEventListener('click', () => {
+    deleteuser(user.id)
+})
+
+
+function deleteuser(id) {
+const url = `http://localhost:5001/api/v1/deletecourse/${id}`;
+console.log(url)
+const requestOptions = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+fetch(url, requestOptions)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((responseData) => {
+      console.log('works')
+  })
+  .catch((error) => {
+    console.error("Fetch error:", error);
+  });
+}
+
+function edituser() {
+
+}
