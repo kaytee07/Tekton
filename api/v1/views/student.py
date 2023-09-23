ff69a7606091f197d0379a53c6a4637cc589d34b
kaytee07 <kofi.taylor07@gmail.com>
2023-09-23 06:25:38 +0000

    limit of number of student per course

--- api/v1/views/student.py
+++ api/v1/views/student.py
@@ -3,7 +3,7 @@
 all user apis including login and logout page
 """
 
-from flask import abort, render_template, request, jsonify, session
+from flask import abort, render_template, request, jsonify, session, flash, redirect, url_for
 from api.v1.views import app_views
 from models import storage
 from models.cohorts import Cohort
@@ -33,9 +33,9 @@ def createstudent():
         if 'phone_no' not in new_data:
             abort(404, description="No phone_no")
         data = new_data.to_dict()
+        cohort_num = 1
         course = storage.get(Course, coursename=data['course_name'])
-        cohort = storage.get(Cohort, cohort_no=1)
-        print(data)
+        cohort = storage.get(Cohort, cohort_no=cohort_num)
         if course and cohort:
             course_dict = course.to_dict()
             cohort_dict = cohort.to_dict()
@@ -43,11 +43,15 @@ def createstudent():
             data['cohort_id'] = cohort_dict['id']
             new_student = Student(**data)
             new_student.save()
+            if course.no_of_students == 20:
+                flash('peak student reached, wait for next cohort or choose different course')
+                return render_template('register.html')
             course.no_of_students = course_dict['no_of_students'] + 1
             cohort.no_of_students = cohort_dict['no_of_students'] + 1
             course.save()
             cohort.save()
-            return jsonify(new_student.to_dict())
+            flash('registered successfully')
+            return render_template('register.html')
     else:
         return render_template('register.html')
 
@@ -57,16 +61,17 @@ def get_all_student(cohort_no, course_name):
     """
     get all student
     """
-    course_dict = storage.get(Course, coursename=course_name).to_dict()
-    cohort_dict = storage.get(Cohort, cohort_no=int(cohort_no)).to_dict()
-    course_id = course_dict['id']
-    cohort_id = cohort_dict['id']
-    all_students = storage.all('Student')
-    student_list = []
-    student_course_cohort = []
-    for value in all_students.values():
-        student_list.append(value.to_dict())
-    for value in student_list:
-        if value['course_id'] == course_id and value['cohort_id'] == cohort_id:
-            student_course_cohort.append(value)
-    return jsonify(student_course_cohort)
+    course = storage.get(Course, coursename=course_name)
+    if course:
+        course_dict = storage.get(Course, coursename=course_name).to_dict()
+        cohort_dict = storage.get(Cohort, cohort_no=int(cohort_no)).to_dict()
+        course_id = course_dict['id']
+        cohort_id = cohort_dict['id']
+        all_students = storage.all('Student')
+        student_course_cohort = []
+        for value in all_students.values():
+            if value.to_dict()['course_id'] == course_id and value.to_dict()['cohort_id'] == cohort_id:
+                student_course_cohort.append(value.to_dict())
+        return jsonify(student_course_cohort)
+    else:
+        return redirect(url_for('appviews.home'))
