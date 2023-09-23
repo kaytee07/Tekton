99126ba20dc8033da54c2a4764ea7e1806ada7ad
kaytee07 <kofi.taylor07@gmail.com>
2023-09-23 10:36:35 +0000

    making select users and cohort dynamic

--- api/v1/static/scripts/home.js
+++ api/v1/static/scripts/home.js
@@ -1,12 +1,11 @@
 student_btn = document.querySelector('form.course_cohort button')
 
-
-
-
 student_btn.addEventListener('click', function (event) {
     event.preventDefault()
     getstudent()
 })
+
+getCohorts()
 
 function getstudent (){
 get_cohort_no = document.querySelector('#cohort').value;
@@ -51,11 +50,93 @@ fetch(url, requestOptions)
            `
       }
       get_std_html.innerHTML = html
-      console.log(retrievedValue)
   })
   .catch((error) => {
     console.error("Fetch error:", error);
   });
 }
 
-getstudent()
+function getCohorts (){
+allcohorts = document.querySelector('select#cohort')
+    
+const url = `http://localhost:5001/api/v1/allcohorts`;
+console.log(url)
+const requestOptions = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+fetch(url, requestOptions)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((responseData) => {
+      console.log("Response data:", responseData);
+      let newData = JSON.stringify(responseData)
+      localStorage.setItem('data', newData)
+      let retrievedValue = localStorage.getItem('data');
+      let html = ""
+      for (let value of responseData) {
+	  console.log(value['cohort_no'])
+	  html += `
+                <option>Cohort ${value['cohort_no']}</option>
+           `
+      }
+      allcohorts.innerHTML = html;
+  })
+   .then((data)=> {
+       getCourses()
+   })
+   .then((data)=> {
+       getstudent()
+   })
+  .catch((error) => {
+    console.error("Fetch error:", error);
+  });
+}
+
+
+function getCourses (){
+allcourses = document.querySelector('select#course')
+    
+const url = `http://localhost:5001/api/v1/allcourses`;
+console.log(url)
+const requestOptions = {
+    method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+fetch(url, requestOptions)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((responseData) => {
+      console.log("Response data:", responseData);
+      let newData = JSON.stringify(responseData)
+      localStorage.setItem('data', newData)
+      let retrievedValue = localStorage.getItem('data');
+      let html = ""
+      for (let value of responseData) {
+	  console.log(value['name'])
+	  html += `
+                <option>${value['name']}</option>
+           `
+      }
+      allcourses.innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("Fetch error:", error);
+  });
+}
+
+
